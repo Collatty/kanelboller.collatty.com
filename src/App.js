3b559f5bca4f3972c7bb4b3b01fba00fa5c145fc
Collatty <hakoncb@gmail.com>
2018-09-29 17:20:17 +0200

    Started adding recepie and images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-import bilde1 from "./assets/kanelbolle1.jpeg";
-import bilde2 from "./assets/kanelboller2.jpeg";
-
 class App extends Component {
   render() {
     return (
@@ -11,6 +8,13 @@ class App extends Component {
           <h1 className="App-title">Her skal det bolles!</h1>
         </header>
         <p className="App-intro" />
+        <h2>Ingerdienser</h2>450g hvetemel <br /> 0,5 dl sukker <br /> ½ pakke
+        fersk gjær <br /> 50 g smeltet smør <br /> 2 dl melk <br /> 1 stk egg{" "}
+        <br /> <br /> <h4>Fyll</h4>
+        100 g smør <br /> 50 g brukt sukker <br /> 1,5 ss kanel <br /> 1 ts salt{" "}
+        <br />
+        <img src={require("./assets/kanelbolle1.jpeg")} align="right" />
+        <img src={require("./assets/kanelboller2.jpeg")} />
       </div>
     );
   }
